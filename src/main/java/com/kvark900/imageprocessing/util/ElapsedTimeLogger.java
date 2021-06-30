004e4da018df1e6ef0a4104dacb17326f1cdecbf
Kvark900 <kvant800@gmail.com>
2021-06-30 17:50:41 +0200

    add ElapsedTimeLogger.java

---
+++ src/main/java/com/kvark900/imageprocessing/util/ElapsedTimeLogger.java
@@ -0,0 +1,26 @@
+package com.kvark900.imageprocessing.util;
+
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
+
+public class ElapsedTimeLogger<T> {
+    private final Runnable runnable;
+    private final Class<T> aClass;
+    private final String operationName;
+
+    public ElapsedTimeLogger(Class<T> aClass, Runnable runnable, String operationName) {
+        this.aClass = aClass;
+        this.runnable = runnable;
+        this.operationName = operationName;
+    }
+
+    public void log()
+    {
+        Log log = LogFactory.getLog(aClass);
+        long start = System.currentTimeMillis();
+        runnable.run();
+        long end = System.currentTimeMillis();
+        long duration = end - start;
+        log.info(String.format("%s has finished after %dms", operationName, duration));
+    }
+}
